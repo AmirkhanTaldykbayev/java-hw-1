b9449947afdfef0eda82c524c85898093c2f190e
Amir <amir_35@mail.ru>
2023-05-29 18:54:56 +0500

    не закончено дз

--- src/main/java/hw2/BubbleSortWithLog.java
+++ src/main/java/hw2/BubbleSortWithLog.java
@@ -4,35 +4,24 @@ package hw2;
 // результат после каждой итерации запишите в лог-файл
 public class BubbleSortWithLog {
 
-    //    public int[] bubble(int[] numbers) {
-//        int[] result = new int[numbers.length];
-//        for (int i = 0; i <= numbers.length + 1; i++) {
-//            for (int j = 0; j < i; j++) {
-//                if (numbers[j] > numbers[j + 1]) {
-//                    toSwap(numbers[j], numbers[j + 1]);
-//                    result[i] = numbers[j];
-//                }
-//            }
-//        }
-//        return result;
-    public int[] bubble(int[] numbers) {
-        int[] result = new int[numbers.length];
-        for (int i = numbers.length - 1; i >= 1; i--) {
-            for (int j = 0; j < i; j++) {
-                if (numbers[j] > numbers[j + 1]) {
-                    toSwap(numbers[j], numbers[j + 1]);
-                }
-                result[i] = numbers[j];
-            }
-        }
-        return result;
-    }
-
-
-    private void toSwap(int num1, int num2) {
+    public void toSwap(int num1, int num2) {
         int temp = num1;
         num1 = num2;
         num2 = temp;
     }
+
+    public void bubbleSort(int[] numbers) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j + 1] < numbers[j]) {
+                    // toSwap(numbers[j], numbers[j + 1]); <-- почему то не работает
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j+1] = temp;
+                }
+            }
+        }
+    }
+
 
 }
--- src/main/java/hw2/Main.java
+++ src/main/java/hw2/Main.java
@@ -8,12 +8,12 @@ public class Main {
 //        String human =  "\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"";
 //        SQL_Request request = new SQL_Request();
 //        System.out.println(request.human(human));
-        //2.
-        int[] numArray = new int[] {1,8,7,6,5,3,4,2,9};
-        System.out.println(Arrays.toString(numArray));
-        BubbleSortWithLog bubbleSort = new BubbleSortWithLog();
-        System.out.println(Arrays.toString(bubbleSort.bubble(numArray)));
-//
+        //2. Решено
+//        int[] numArray = new int[] {1,8,7,6,5,3,4,2,9};
+//        BubbleSortWithLog bubbleSort = new BubbleSortWithLog();
+//        bubbleSort.bubbleSort(numArray);
+//        System.out.println(Arrays.toString(numArray));
+
 
         }
     }
