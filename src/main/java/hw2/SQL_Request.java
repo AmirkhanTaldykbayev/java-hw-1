17dd6a18c4bc798e168a086beddc9124a6f17333
Amir <amir_35@mail.ru>
2023-06-16 01:36:16 +0500

    1, 2

--- src/main/java/hw2/SQL_Request.java
+++ src/main/java/hw2/SQL_Request.java
@@ -1,14 +1,5 @@
 package hw2;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-
 // 1) Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса,
 // используя StringBuilder. Данные для фильтрации приведены ниже в виде json-строки.
 // Если значение null, то параметр не должен попадать в запрос.
